4085768dbdeaa316f750bea0403b89f8d9f22d8f
alexandro <alexandrojorgeantonio@gmail.com>
2023-09-03 12:47:40 -0300

    finalizado

--- NumeroAleatorioGame/Program.cs
+++ NumeroAleatorioGame/Program.cs
@@ -10,6 +10,41 @@ namespace NumeroAleatorioGame
     {
         static void Main(string[] args)
         {
+            jogo();
+            Console.ReadKey();
+        }
+        static void jogo()
+        {
+            int tentativas = 0;
+            int num1 = 1;
+            int num2 = 10;
+            Random random = new Random();
+            int numero_aleatorio = random.Next(num1, num2);
+            Console.Write("Clique Enter para começar");
+            Console.ReadLine();
+            int jogador = 0;
+            while (numero_aleatorio != jogador && tentativas < 5)
+            {
+                Console.WriteLine($"Digite um numero de {num1} a {num2}: ");
+                jogador = int.Parse(Console.ReadLine());
+                tentativas++;
+                if (numero_aleatorio < jogador)
+                {
+                    Console.WriteLine("Jogada muito alta,tente novamente");
+                   
+                }
+                else if(numero_aleatorio > jogador)
+                {
+                    Console.WriteLine("Jogada muito baixa,tente novamente");
+                   
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            Console.WriteLine($"Voce ganhou\nNumero escolhido {jogador}\nComputador escolheu: {numero_aleatorio}");
         }
     }
 }
